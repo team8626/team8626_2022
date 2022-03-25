6f9315a9422a7f4bd284da5db221afb82b295d11
nedf123 <nedfinkle@student.scit.org>
2022-03-25 18:48:42 -0400

    Add Quick Routine for Autonomous

---
+++ src/main/java/frc/robot/commands/ShootAndMove.java
@@ -0,0 +1,63 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+// Java Libraries
+// import java.util.function.DoubleSupplier;
+
+// WPI Library dependencies
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+// Team8626 Libraries
+import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.StorageSubsystem;
+//import frc.robot.commands.StoreCargo;
+
+/**
+ * Get the robot ready to collect Cargo.
+ *      - Get Intake Out
+ *      - Start Loading the Front Storage Unit
+ * 
+ * If the Front Storage is already in use, this will do nothing.
+ **/
+public class PrepareToCollect extends ParallelCommandGroup {
+  // @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
+  private final IntakeSubsystem  m_intake;
+  private final StorageSubsystem m_storage;
+
+  /**
+   * Creates a new PrepareToCollect command.
+   * 
+   * @param intake  The Intake
+   * @param storage The Storage
+   */
+  public PrepareToCollect(IntakeSubsystem intake, StorageSubsystem storage) {
+    m_intake = intake;
+    m_storage = storage;
+
+    addCommands(
+        // Activate the Intake
+        new InstantCommand(m_intake::activate, m_intake),
+
+        // Activate the Storage for loading
+        new StoreCargo(m_storage));
+  }
+
+  @Override
+  public boolean isFinished() {
+    boolean ret_value = false;
+    if(m_storage.isFull() == true) {
+      ret_value = true;
+    }
+    return ret_value;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  public void end(boolean interrupted) {
+    // Deactivate the Intake
+    new InstantCommand(m_intake::deactivate, m_intake);
+  }
+}
