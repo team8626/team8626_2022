45807bfc70341d4e087cd5b3489088915c3b3e5d
git_master <team8626.sw@gmail.com>
2022-04-08 00:16:24 -0400

    Add println infos...

--- src/main/java/frc/robot/commands/TurnDegreesCommand.java
+++ src/main/java/frc/robot/commands/TurnDegreesCommand.java
@@ -26,13 +26,17 @@ public class TurnDegreesCommand extends PIDCommand {
    * @param drivetrain
    */
   
-  public TurnDegreesCommand(DoubleSupplier angleDegrees, DriveSubsystem drivetrain) {
+  public TurnDegreesCommand(DoubleSupplier angleDegrees, DoubleSupplier power, DriveSubsystem drivetrain) {
 
-    super( new PIDController(4, 0, 0), drivetrain::getHeading, angleDegrees, rot -> drivetrain.arcadeDrive(0, rot));
+    super( new PIDController(4, 0, 0), drivetrain::getHeading, angleDegrees, rot -> drivetrain.arcadeDrive(0, rot * power.getAsDouble()));
 
     m_drivetrain = drivetrain;  
     addRequirements(m_drivetrain);
     
+    System.out.println("[TURN_DEGREES] Start Turning "+ angleDegrees.getAsDouble() + "deg");
+
+    // Set the controller to be continuous (because it is an angle controller)
+    getController().enableContinuousInput(-180, 180);
     getController().setTolerance(0.01);
   }
 
@@ -41,10 +45,12 @@ public class TurnDegreesCommand extends PIDCommand {
   public void initialize() {
     m_drivetrain.zeroHeading();
     super.initialize();
+
   }
   
   @Override
   public boolean isFinished() {
+    System.out.println("[TURN_DEGREES] Done Turning ");
     return getController().atSetpoint();
   }
 }
